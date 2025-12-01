10990d46d4b26b89ef3b0a61996da71beac47c2c
Kaan Karaca <kaan94karaca@gmail.com>
2025-12-01 23:43:07 +0300

    sidebar

--- src/renderer/src/components/team-switcher.tsx
+++ src/renderer/src/components/team-switcher.tsx
@@ -25,7 +25,6 @@ export function TeamSwitcher({
   teams: {
     name: string
     logo: React.ElementType
-    plan: string
   }[]
 }) {
   const { isMobile } = useSidebar()
@@ -48,10 +47,9 @@ export function TeamSwitcher({
                 <activeTeam.logo className="size-4" />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">
+                <span className="truncate font-normal">
                   {activeTeam.name}
                 </span>
-                <span className="truncate text-xs">{activeTeam.plan}</span>
               </div>
               <ChevronsUpDown className="ml-auto" />
             </SidebarMenuButton>
