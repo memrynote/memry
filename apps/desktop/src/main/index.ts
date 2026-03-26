bd21b944512f45ae0231b8a1812eefaceee7b06f
Kaan Karaca <kaan94karaca@gmail.com>
2026-03-26 05:19:13 +0300

    fix(desktop): quit app on all windows closed regardless of platform

--- apps/desktop/src/main/index.ts
+++ apps/desktop/src/main/index.ts
@@ -871,13 +871,8 @@ app.on('before-quit', (event) => {
     })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit()
 })
 
 // Unregister all global shortcuts when the app is about to quit
