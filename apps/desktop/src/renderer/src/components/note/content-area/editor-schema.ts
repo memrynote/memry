a65d5c3ba191809a17436d242e0f5437d301da47
Kaan Karaca <kaan94karaca@gmail.com>
2026-04-04 19:51:26 +0300

    feat: integrate link mentions and YouTube embeds into editor
    Register linkMention + youtubeEmbed in editor schema. Wire paste link
    menu handlers in ContentArea for mention/embed conversion. Hydrate
    link mention favicons on document load via use-editor-sync.

--- apps/desktop/src/renderer/src/components/note/content-area/editor-schema.ts
+++ apps/desktop/src/renderer/src/components/note/content-area/editor-schema.ts
@@ -7,20 +7,24 @@ import {
 import { codeBlockOptions } from '@blocknote/code-block'
 import { createFileBlock } from './file-block'
 import { createCalloutBlock } from './callout-block'
+import { createYoutubeEmbedBlock } from './youtube-embed-block'
 import { WikiLink } from './wiki-link'
 import { HashTag } from './hash-tag'
+import { LinkMention } from './link-mention'
 
 export const editorSchema = BlockNoteSchema.create({
   blockSpecs: {
     ...defaultBlockSpecs,
     codeBlock: createCodeBlockSpec(codeBlockOptions),
     file: createFileBlock(),
-    callout: createCalloutBlock()
+    callout: createCalloutBlock(),
+    youtubeEmbed: createYoutubeEmbedBlock()
   },
   inlineContentSpecs: {
     ...defaultInlineContentSpecs,
     wikiLink: WikiLink,
-    hashTag: HashTag
+    hashTag: HashTag,
+    linkMention: LinkMention
   }
 })
 
