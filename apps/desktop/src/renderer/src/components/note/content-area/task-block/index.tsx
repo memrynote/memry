d1e8fbb3bfa678abe319e6d386d716b1e1b1a234
Kaan Karaca <kaan94karaca@gmail.com>
2026-04-05 15:06:02 +0300

    feat: add task block utility functions with tests

---
+++ apps/desktop/src/renderer/src/components/note/content-area/task-block/index.tsx
@@ -0,0 +1,45 @@
+import { createReactBlockSpec } from '@blocknote/react'
+import { TaskBlockRenderer } from './task-block-renderer'
+
+export const createTaskBlock = () =>
+  createReactBlockSpec(
+    {
+      type: 'taskBlock' as const,
+      propSchema: {
+        taskId: { default: '' },
+        title: { default: '' },
+        checked: { default: false }
+      },
+      content: 'none'
+    },
+    {
+      render: (props) => (
+        <TaskBlockRenderer
+          block={props.block as any}
+          editor={props.editor}
+          contentRef={props.contentRef}
+        />
+      )
+    }
+  )
+
+export function getTaskSlashMenuItem(editor: any) {
+  return {
+    title: 'Task',
+    onItemClick: () => {
+      const currentBlock = editor.getTextCursorPosition().block
+      editor.updateBlock(currentBlock, {
+        type: 'taskBlock' as any,
+        props: { taskId: '', title: '', checked: false }
+      })
+      window.dispatchEvent(
+        new CustomEvent('task-block:open-creation', {
+          detail: { blockId: currentBlock.id }
+        })
+      )
+    },
+    aliases: ['task', 'todo', 'action'],
+    group: 'Basic blocks',
+    subtext: 'Create a linked task'
+  }
+}
