9e678f670680c1a1eb800d7088e117ba5254c79e
Kaan Karaca <kaan94karaca@gmail.com>
2026-04-21 17:58:35 +0300

    Update sidebar navigation icons

--- apps/desktop/src/renderer/src/lib/icons/sidebar-nav-icons.tsx
+++ apps/desktop/src/renderer/src/lib/icons/sidebar-nav-icons.tsx
@@ -1,4 +1,5 @@
 import { forwardRef } from 'react'
+import { Book2, CheckSquare3, Inbox } from './icon-map'
 import type { AppIcon } from './types'
 
 type SvgProps = React.ComponentPropsWithRef<'svg'> & {
@@ -28,23 +29,9 @@ function createSvgIcon(path: React.ReactNode, displayName: string, viewBox = '0 
   return Icon as AppIcon
 }
 
-export const SidebarInbox = createSvgIcon(
-  <>
-    <path d="M1.5 9.5h3.5l1 2h3l1-2h3.5" />
-    <path d="M2.5 4.5l-1 5v3h12v-3l-1-5h-10z" />
-  </>,
-  'SidebarInbox'
-)
+export const SidebarInbox = Inbox
 
-export const SidebarJournal = createSvgIcon(
-  <>
-    <rect x="3" y="1.5" width="9.5" height="12" rx="1" />
-    <path d="M5.5 4.5h4" />
-    <path d="M5.5 7h3" />
-    <path d="M1.5 3.5v8" />
-  </>,
-  'SidebarJournal'
-)
+export const SidebarJournal = Book2
 
 export const SidebarCalendar = createSvgIcon(
   <>
@@ -60,13 +47,7 @@ export const SidebarCalendar = createSvgIcon(
   'SidebarCalendar'
 )
 
-export const SidebarTasks = createSvgIcon(
-  <>
-    <rect x="2" y="2" width="11" height="11" rx="2" />
-    <path d="M5 7.5l1.5 1.5 3.5-3.5" />
-  </>,
-  'SidebarTasks'
-)
+export const SidebarTasks = CheckSquare3
 
 export const SidebarGraph = createSvgIcon(
   <>
