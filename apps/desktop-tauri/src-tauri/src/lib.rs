d3553eb21ca505165ac78b19120bbd5ae7775eec
Kaan Karaca <kaan94karaca@gmail.com>
2026-04-26 03:29:07 +0300

    m3(commands): vault_* command surface (open/close/list/read/write/...) + shell/dialog stubs

--- apps/desktop-tauri/src-tauri/src/lib.rs
+++ apps/desktop-tauri/src-tauri/src/lib.rs
@@ -34,6 +34,31 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .manage(app_state)
+        .invoke_handler(tauri::generate_handler![
+            commands::settings::settings_get,
+            commands::settings::settings_set,
+            commands::settings::settings_list,
+            commands::vault::vault_open,
+            commands::vault::vault_close,
+            commands::vault::vault_get_status,
+            commands::vault::vault_get_current,
+            commands::vault::vault_get_all,
+            commands::vault::vault_switch,
+            commands::vault::vault_remove,
+            commands::vault::vault_get_config,
+            commands::vault::vault_update_config,
+            commands::vault::vault_list_notes,
+            commands::vault::vault_read_note,
+            commands::vault::vault_write_note,
+            commands::vault::vault_delete_note,
+            commands::vault::vault_reveal,
+            commands::vault::vault_reindex,
+            commands::shell::shell_open_url,
+            commands::shell::shell_open_path,
+            commands::shell::shell_reveal_in_finder,
+            commands::dialog::dialog_choose_folder,
+            commands::dialog::dialog_choose_files,
+        ])
         .setup(|_app| {
             tracing_subscriber::fmt()
                 .with_env_filter(
@@ -45,12 +70,6 @@ pub fn run() {
             tracing::info!("memry desktop-tauri booting (m2 settings slice)");
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![
-            commands::settings::settings_get,
-            commands::settings::settings_set,
-            commands::settings::settings_list,
-            commands::lifecycle::notify_flush_done,
-        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
